90c485e98c1ea8f74fa3f4d34bc2c959cbf5b7f2
charitha-meka <mekacharitha.19@gmail.com>
2024-03-30 19:53:38 -0400

    Remove deleted files

--- src/app/models/Category.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model, models } from "mongoose";
-
-const CategorySchema = new Schema({
-  name: { type: String, require: true, unique: true },
-  image: { type: String },
-}, { timestamps: true });
-
-export const Category = models?.Category || model('Category', CategorySchema);
